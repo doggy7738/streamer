2cf3b1f6a031135aecc2d6d631e2a935322a1644
Preston-code offcial <123032424+doggy7738@users.noreply.github.com>
2025-05-18 00:36:25 -0700

    Update AdminPage.tsx

--- src/pages/admin/AdminPage.tsx
+++ src/pages/admin/AdminPage.tsx
@@ -13,7 +13,7 @@ export function AdminPage() {
     <SubPageLayout>
       <ThinContainer>
         <Heading1>Admin tools</Heading1>
-        <Paragraph>Silly tools used test P-Stream! ૮₍´˶• . • ⑅ ₎ა</Paragraph>
+        <Paragraph>used to test</Paragraph>
 
         <ConfigValuesPart />
         <BackendTestPart />
